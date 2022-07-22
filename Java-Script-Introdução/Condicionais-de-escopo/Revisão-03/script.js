661b9e9bd81611b171f71d2ab7f52569d1a199d5
Rony_Fonseca <92323447+RonyFonseca@users.noreply.github.com>
2022-07-22 11:58:32 -0300

    update

--- Java-Script-Introdução/Condicionais-de-escopo/Revisão-03/script.js
+++ Java-Script-Introdução/Condicionais-de-escopo/Revisão-03/script.js
@@ -7,58 +7,56 @@ let carro = {
     estado: "novo"
 };
 
+
 function mudarPropriedade(nomePropriedade, novoValor){
     switch(nomePropriedade){
         // O case rodas é um exemplo para as próximas partes
         case "rodas":
             if(novoValor > 1){
-                carro.rodas = novoValor;   
+                carro.rodas = novoValor;    
             }
             else{
                 return "Minimo de rodas precisa ser 2";
             }
             break;
         case "portas":
+            // Para alterar as portas, é necessário que o
+            // novo valor seja maior que 0
             if(novoValor > 0){
-                carro.portas = novoValor; 
-            }else{
-                return "Minimo de rodas precisa ser 1"
+                carro.portas = novoValor;
             }
-            break;
-        case "cor":
-            // para alterar a cor, novo valor pode ser igual a cinza ou preto ou vermelho
-            // se for diferente, retornar "Cor invalida"
-            if(novoValor == "cinza", "preto", "vermelho"){
-                carro.cor = novoValor;
-            }else {
-                return "Cor invalida";
-            }
-            break;
-            
-        case "ano":
-            if(novoValor == 2019){
-                carro.ano = novoValor
-                if(novoValor == 2019){
-                    carro.estado == "novo";
-                }else{
-                    return "Usado";
-                }
-            }
+            else{
+                console.log("Minimo de portas precisa ser 1");
+            };
+            // senão deve retornar a string "Minimo de portas precisa ser 1"
             break;
 
+        case "cor":
+            if(novoValor == "vermelho","cinza","preto"){
+                carro.cor = novoValor;
+            }
+            else{
+                return "cor invalida";
+            };
+            // para alterar a cor, novo valor pode ser igual a cinza ou preto ou vermelho
+            // se for diferente, retornar "Cor invalida"
+            break;
         // criar um case para o "ano"
+        case "ano":
+            if(novoValor == 2019){
+                carro.ano = "novo";
+            }
+            else{
+                carro.ano = "usado";
+            }
         // alterar o ano do carro para o ano informado
         // se o ano for igual a 2019, alterar o estado para "novo" 
         // senao alterar o estado para "usado"
         default:
-            return "Propriedade invalida"; 
+            return "Propriedade invalida";
             // retornar a fraase "Propriedade invalida";
     }
-
-    return "Alteracao concluida"
+    return "Alteracao concluida";
     // retornar a frase Alteracao concluida somente se foi possivel
     // alterar um dado do objeto.
 }
-
-
-mudarPropriedade("portas", -1);
