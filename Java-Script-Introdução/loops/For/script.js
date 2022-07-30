2c9c0fb600de7c582189d1b1b5707242f33e2dbb
Rony_Fonseca <92323447+RonyFonseca@users.noreply.github.com>
2022-07-30 12:48:57 -0300

    update

---
+++ Java-Script-Introdução/loops/For/script.js
@@ -0,0 +1,17 @@
+for (let x =1; x <= 10; x++){
+    console.log(x);
+};
+
+console.log('-----------');
+
+let y = 1; 
+for(; y <= 10; y++){
+    console.log(y);
+};
+
+console.log('-----------');
+
+let array = ["Gato", "Cachorro", "Rony"];
+for(;array.length; array++){
+    console.log(array);
+}
