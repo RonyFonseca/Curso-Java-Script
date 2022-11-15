007c919b610327294c8e6142e642993ffbd13649
Rony_Fonseca <92323447+RonyFonseca@users.noreply.github.com>
2022-11-15 20:17:31 -0300

    update

--- Java-Script-Introdução/loops/while/treino/script.js
+++ Java-Script-Introdução/loops/while/treino/script.js
@@ -1,6 +1,14 @@
-let num = 1; 
+let num = 1;
 
-while(num <= 10){
-    console.log(num); 
-    num ++; 
-};
+while(num<=10){
+    console.log("Número " + num);
+    num = num *2;
+}
+
+let num2 = 1;
+
+do {
+    console.log(num2);
+    num2++;
+}
+while(num2<10);
